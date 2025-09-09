238933dbb80bbf104aee7180a2ef8d86be70bd56
Bharath D Singh <bharathsingh657@gmail.com>
2025-09-09 22:31:22 +0530

    Added solution for fizzbuzz problem

---
+++ leetcode/fizzbuzz.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+
+        vector<string> result;
+
+       for(int i=1;i<=n;i++)
+       {    
+            string s = to_string(i);
+
+            if ((i%3==0)&&(i%5==0))
+            {
+                result.push_back("FizzBuzz");
+            }
+
+            else if (i%3==0)
+            {
+                result.push_back("Fizz");
+            }
+
+             else if (i%5==0)
+            {
+                result.push_back("Buzz");
+            }
+
+            else
+            {
+                result.push_back(s);
+            }
+       } 
+       return result;
+        
+    }
+};
